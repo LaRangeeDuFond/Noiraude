4e2682bb571ccb5a19cec643084e8bc4b804f1d6
Maxime Grange <maxime.grange@numericable.fr>
2014-12-04 16:28:53 +0100

    erreur

---
+++ UnityNoireaude/Assets/Scripts/move.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	public Transform aim1,aim2;
+
+	void Awake ()
+	{
+		transform.position= aim2.position;
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if(transform.position == aim1.position)
+		{
+			while(transform.position!=aim2.position)
+			{
+				transform.Translate(Vector3.MoveTowards (transform.position,aim2.position,0.5f));
+			}
+		}
+		if(transform.position == aim2.position)
+		{
+			while(transform.position!=aim1.position)
+			{
+				transform.Translate(Vector3.MoveTowards (transform.position,aim1.position,0.5f));
+			}
+		}
+	
+	}
+}
